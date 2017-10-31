ddb6409390d28c1c9e9b7181e50c3fa33679def7
Ashish Malav <aashmalav@gmail.com>
2017-10-31 16:03:31 -0500

    defined remove function

--- Data Structures Problems/List/listDataStructure.js
+++ Data Structures Problems/List/listDataStructure.js
@@ -9,6 +9,30 @@ class List {
     this.dataStore[this.listSize++] = element;
   }
 
+  //find the index of the element in the dataStore, if present returns index otherwise returns -1
+  find(element) {
+    for (let i = 0; i < this.dataStore.length; i++){
+      if (this.dataStore[i] === element) return i;
+    }
+
+    return -1;
+  }
+
+  //remove (element) - Removes element from list
+  /*The remove() function uses the position returned by find() to splice the dataStore array at that place.
+   After the array is modified, listSize is decremented by 1 to reflect the new size of the list.
+   The function returns true if an element is removed, and false otherwise.*/
+  remove(element) {
+    const index = find(element);
+
+    if (index >= 0){
+      this.dataStore.splice(index, 1);
+      --this.listSize;
+      return true;
+    }
+
+    return false;
+  }
 }
 
 const list = new List();
