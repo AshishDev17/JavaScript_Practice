24cec663b8b1afccc17192af93325e550ecc123c
Ashish Malav <aashmalav@gmail.com>
2017-10-31 15:50:58 -0500

    defined append function

--- Data Structures Problems/List/listDataStructure.js
+++ Data Structures Problems/List/listDataStructure.js
@@ -1,4 +1,19 @@
-function List(){
-  this.listSize = 0;
-  this.dataStore = [];
+class List {
+  constructor() {
+    this.listSize = 0;
+    this.dataStore = [];
+  }
+
+  //append (element) - Adds new element to end of list
+  append(element) {
+    this.dataStore[this.listSize++] = element;
+  }
+
 }
+
+const list = new List();
+
+
+
+
+console.log('list ', list);
