fa47d8e3eab43867164602296850cd672361ecbb
Ashish Malav <aashmalav@gmail.com>
2017-11-03 14:34:06 -0500

    defined peek, clear, length functions of stack ADT

---
+++ Data Structures Problems/Stack/stackADT.js
@@ -0,0 +1,32 @@
+class Stack {
+  constructor() {
+    this.top = 0;
+    this.dataStore = [];
+  }
+
+  //push() function adds the element to the top of the stack
+  push(element) {
+    this.dataStore[this.top++] = element;
+  }
+
+  //pop() function returns the element in the top position of the stack
+  pop() {
+    return this.dataStore[--this.top];
+  }
+
+  //peek() function returns the value stored at the top of a stack without removing it from the stack
+  peek() {
+   return this.dataStore[this.top - 1];
+  }
+
+  //clear() function clears the stack
+  clear() {
+    this.top = 0;
+    this.dataStore = [];
+  }
+
+  //length() function returns number of elements stored in stack
+  length() {
+    return this.top;
+  }
+}
