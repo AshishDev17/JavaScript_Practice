5ba19e52d66cfd6a03e09093a2e41adbed754e58
Ashish Malav <aashmalav@gmail.com>
2017-11-04 15:37:08 -0500

    defined Queue DS using Array built in methods

--- Data Structures Problems/Queue/queueDS.js
+++ Data Structures Problems/Queue/queueDS.js
@@ -1,30 +1,28 @@
+//Define Queue DS using Array built in methods
 class Queue {
   constructor() {
     this.dataStore = [];
-    this.head = 0;
-    this.tail = 0;
   }
 
   //enqueue() function adds an element to the end of a queue
   enqueue(element) {
-    this.dataStore[this.tail++] = element;
+    this.dataStore.push(element);
   }
 
   //dequeue() function removes an element from the front of a queue
   dequeue() {
-    this.dataStore.shift();
-    return this.dataStore[this.head++];
+    return this.dataStore.shift();
   }
 
   //front() function returns the front element of the queue
   front() {
-    return this.dataStore[this.head];
+    return this.dataStore[0];
   }
 
 
   //back() function returns the back element of the queue
   back() {
-    return this.dataStore[this.tail - 1];
+    return this.dataStore[this.dataStore.length - 1];
   }
 
   //toString() function displays all the elements in a queue
@@ -33,12 +31,12 @@ class Queue {
   }
 
   size() {
-    return this.tail - this.head;
+    return this.dataStore.length;
   }
 
   //empty()function lets us know if a queue is empty
   isEmpty() {
-    if (this.tail - this.head <= 0) return true;
+    if (this.dataStore.length === 0) return true;
 
     return false;
   }
@@ -46,10 +44,63 @@ class Queue {
   //clear() function clears the queue
   clear() {
     this.dataStore = [];
-    this.head = 0;
-    this.tail = 0;
   }
 }
+
+
+//Define Queue DS using indexers
+// class Queue {
+//   constructor() {
+//     this.dataStore = [];
+//     this.head = 0;
+//     this.tail = 0;
+//   }
+
+//   //enqueue() function adds an element to the end of a queue
+//   enqueue(element) {
+//     this.dataStore[this.tail++] = element;
+//   }
+
+//   //dequeue() function removes an element from the front of a queue
+//   dequeue() {
+//     this.dataStore.shift();
+//     return this.dataStore[this.head++];
+//   }
+
+//   //front() function returns the front element of the queue
+//   front() {
+//     return this.dataStore[this.head];
+//   }
+
+
+//   //back() function returns the back element of the queue
+//   back() {
+//     return this.dataStore[this.tail - 1];
+//   }
+
+//   //toString() function displays all the elements in a queue
+//   toString() {
+//     return this.dataStore.toString();
+//   }
+
+//   size() {
+//     return this.tail - this.head;
+//   }
+
+//   //empty()function lets us know if a queue is empty
+//   isEmpty() {
+//     if (this.tail - this.head <= 0) return true;
+
+//     return false;
+//   }
+
+//   //clear() function clears the queue
+//   clear() {
+//     this.dataStore = [];
+//     this.head = 0;
+//     this.tail = 0;
+//   }
+// }
 
 var q = new Queue();
 q.enqueue('Meredith');
