0cefb75676e3ed1065f68d0427951127790a913d
Ashish Malav <aashmalav@gmail.com>
2017-11-06 13:18:18 -0600

    defined addFirst and addLast  functions for Doubly Linked List DS

--- Data Structures Problems/Linked List/doublyLinkedListDS.js
+++ Data Structures Problems/Linked List/doublyLinkedListDS.js
@@ -23,4 +23,34 @@ class DoublyLinkedList {
 
     return null;
   }
+
+  //addFirst() or addToHead() function accepts an argument 'element', and add node of value equal to element at the head of LL
+  addFirst(element) {
+    let node = new Node(element);
+
+    if (this.head) {
+      this.head.previous = node;
+      node.next = this.head;
+    }
+    else {
+      this.tail = node;
+    }
+
+    this.head = node;
+  }
+
+  //addLast() or addToTail() function accepts an argument 'element', and add node of value equal to element at the tail of LL
+  addLast(element) {
+    let node = new Node(element);
+
+    if (this.tail) {
+      this.tail.next = node;
+      node.previous = this.tail;
+    }
+    else {
+      this.head = node;
+    }
+
+    this.tail = node;
+  }
 }
