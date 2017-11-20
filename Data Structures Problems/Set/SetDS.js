0867a09c271bccc7eecaa7feb68dfc27fec1817f
Ashish Malav <aashmalav@gmail.com>
2017-11-20 17:32:18 -0600

    defined has and delete functions for Set DS

--- Data Structures Problems/Set/SetDS.js
+++ Data Structures Problems/Set/SetDS.js
@@ -3,11 +3,28 @@ class Set {
     this.store = {};
   }
 
-  //add() function accepts an argument'value', and adds to the store. Add() function returns true if value is
-  //added to the store, otherwise returns false
+  //has() function accepts an argument 'value', and returns true if 'value' is present in the set, otherwise
+  //returns false
+  has(value) {
+    return this.store.hasOwnProperty(value);
+  }
+
+  //add() function accepts an argument'value', and adds to the set. Add() function returns true if the 'value' is
+  //added to the set, otherwise returns false
   add(value) {
-    if (!this.store[value]) {
+    if (!this.store.has(value)) {
       this.store[value] = value;
+      return true;
+    }
+
+    return false;
+  }
+
+  //delete() function accepts an argument'value', and deletes that value from the set. Delete() function returns true if the 'value' is
+  //deleted from the set, otherwise returns false
+  delete(value) {
+    if (this.store.has(value)) {
+      delete this.store[value];
       return true;
     }
 
