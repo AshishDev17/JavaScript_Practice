a3127ef54347e3ae7ad7f87fba055fedb59cb5b1
AshishDev17 <aashmalav@gmail.com>
2017-12-13 00:18:45 -0600

    updated OOPS.js

--- OOPS/OOPS.js
+++ OOPS/OOPS.js
@@ -1,14 +1,14 @@
 
 /*
   CLASS: It is a template that defines what characterstics an object should have.
-  INSTANTIATION: It is the process of creating an object instance from a class is called instantiation.
+  INSTANTIATION: The process of creating an object instance from a class is called instantiation.
 
 */
 
 /*
   ENCAPSULATION: Storing properties and functions inside an object so that we can structure and access them
 
-  ABSTRACTION: Ceating a simple model of a more complex thing that represents its more important aspects in a way that is easy to work with for programming
+  ABSTRACTION: Ceating a simple model of a more complex thing that represents its more important aspects in a way that is easy to work with while programming
 */
 
 //Define a Mammal Parent class
@@ -33,10 +33,10 @@ Mammal.prototype.haveBaby = function(){
 /*
   INHERITANCE: New class can be created based on other class, which is a Parent Class, and new class is known as Child Class. Child class inherits the data and code features of its Parent Class. Inheritance helps to reuse functinality common to all classes, rather than having to duplicate it. We can then define specialized features in Child Class as defined.
 
-  POLMORPHISM: We can also define the same feature separately in different classes, as each definition of that feature is in different namespace. This ability of implementing same feature separately in different classes is know as Polymorphism.
+  POLYMORPHISM: We can also define the same feature separately in different classes, as each definition of that feature is in different namespace. This ability of implementing same feature separately in different classes is know as Polymorphism.
 */
 
-//Define a Cat Child Class of Person Class
+//Define a Cat Child Class of Mammal Class
 function Cat(name, color){
     Mammal.call(this, name);
     this.color = color;
