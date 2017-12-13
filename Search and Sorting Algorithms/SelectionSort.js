5a244deb73446bb350021a21defdfa7bf5042317
AshishDev17 <aashmalav@gmail.com>
2017-12-13 00:26:14 -0600

    added comment in SelectionSort.js

--- Search and Sorting Algorithms/SelectionSort.js
+++ Search and Sorting Algorithms/SelectionSort.js
@@ -2,6 +2,7 @@
   Selection sort works by starting at the beginning of the array and comparing the first element with the remaining elements. After examining all the elements, the smallest element is placed in the first position of the array, and the algorithm moves to the second position. This process continues until the algorithm arrives at the next to last position in the array, at which point all the data is sorted.
  */
 
+//swap() function accepts three arguments, 'array', 'index1' and 'index2', and swaps the elements of 'array' at 'index1' and 'index2'
 const swap = (array, index1, index2) => {
   const temp = array[index1];
   array[index1] = array[index2];
